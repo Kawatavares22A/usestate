e119305256056f1ec93c1f4b62399ada5181e918
kawa556622 <kawa556622@gmail.com>
2025-05-25 13:43:40 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./Components/Header"
+import { Exemplo1 } from "./Components/Exemplo1"
+import { Exemplo2 } from "./Components/Exemplo2"
+
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Exemplo1 />
+        <Exemplo2 />
+      </main>
+    </>
+  )
+}
